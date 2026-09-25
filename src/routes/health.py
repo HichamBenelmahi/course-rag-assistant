5a5c8bc103d2134b8e57c0f2395565ef68dc30c9
Hicham Benelmahi <88251382+HichamBenelmahi@users.noreply.github.com>
2026-09-25 10:46:33 +0000

    Ajoute un message à la route /health

--- src/routes/health.py
+++ src/routes/health.py
@@ -4,4 +4,4 @@ health_router = APIRouter()
 
 @health_router.get("/health")
 async def health() -> dict[str,str]:
-    return{"message":"asmaaae meryem je vous aiiiiiiimes"}
+    return{"status":"ok"}
